4fe4f507b11802538a091642beba4d1a8d1ef8dc
D-s15 <65296932+D-s15@users.noreply.github.com>
2020-05-21 15:13:57 +0100

    update project

---
+++ appAndroid/app/src/main/java/pt/ipbeja/estig/ip/monumentosdiocesebeja/criarContaUtilizador.java
@@ -0,0 +1,21 @@
+package pt.ipbeja.estig.ip.monumentosdiocesebeja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class criarContaUtilizador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criar_conta_utilizador);
+
+    }
+
+    public void onClick(){
+
+    }
+}
---
+++ appAndroid/app/src/main/java/pt/ipbeja/estig/ip/monumentosdiocesebeja/utilizador.java
@@ -0,0 +1,39 @@
+package pt.ipbeja.estig.ip.monumentosdiocesebeja;
+
+public class utilizador {
+
+    private long id;
+    private String email;
+    private String password;
+
+
+    public utilizador(long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
